26211aeb228ea0810339abbda5f9f4d60c1b5c3f
장현우 <wkdgusdn3@gmail.com>
2015-03-07 16:11:08 +0900

    역 카운트 기능 timer 기능 추가
    Issue
    각역마다 시간 반영
    Thread 오류 수정해야함

--- gen/com/example/timesubway/R.java
+++ gen/com/example/timesubway/R.java
@@ -1653,12 +1653,13 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090042;
+        public static final int action_settings=0x7f090045;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
+        public static final int count=0x7f090042;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
         public static final int departure=0x7f090040;
@@ -1700,8 +1701,10 @@ containing a value of this type.
         public static final int showTitle=0x7f090012;
         public static final int spinner=0x7f09003f;
         public static final int split_action_bar=0x7f090004;
+        public static final int start=0x7f090044;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
+        public static final int timer=0x7f090043;
         public static final int title=0x7f090027;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
--- src/com/example/timesubway/MainActivity.java
+++ src/com/example/timesubway/MainActivity.java
@@ -1,76 +1,179 @@
 package com.example.timesubway;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-	
+
 	String[] lines;
 	String[] line1;
 	String[] line2;
+	int station_count;
+	int minute;
+	int second = 0;
+	Handler handler;
+
+	ArrayAdapter<String> lines_adapter;
 	ArrayAdapter<String> line1_adapter;
 	ArrayAdapter<String> line2_adapter;
+	Spinner lines_sp;
 	Spinner departure_sp;
 	Spinner destination_sp;
-	
+	TextView count;
+	TextView timer;
+	Button start;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		count = (TextView)findViewById(R.id.count);
+		timer = (TextView)findViewById(R.id.timer);
+		start = (Button)findViewById(R.id.start);	
 		
+		handler = new Handler();
+
+		// spinner 등록
+		lines_sp = (Spinner) this.findViewById(R.id.spinner);
+		departure_sp = (Spinner) this.findViewById(R.id.departure);
+		destination_sp = (Spinner) this.findViewById(R.id.destination);
+
+		setLine();
+		setSpinnerAdapter();
+
+		// spinner adapter 연결
+		lines_sp.setAdapter(lines_adapter);
+
+		setSpinnerListener();
+	}
+
+	void setLine() {
+		// 정거장 등록
 		lines = getResources().getStringArray(R.array.lines);
 		line1 = getResources().getStringArray(R.array.line1);
 		line2 = getResources().getStringArray(R.array.line2);
+	}
 
-		ArrayAdapter<String> lines_adapter = new ArrayAdapter<String>
-		(this, android.R.layout.simple_spinner_dropdown_item, lines); 
-		Spinner lines_sp = (Spinner) this.findViewById(R.id.spinner);
-		lines_sp.setAdapter(lines_adapter);
-		
-		departure_sp = (Spinner) this.findViewById(R.id.departure);
-		destination_sp = (Spinner) this.findViewById(R.id.destination);
-		
+	void setSpinnerAdapter() {
+		// spinner adapter 등록
+		lines_adapter = new ArrayAdapter<String>
+		(this, android.R.layout.simple_spinner_dropdown_item, lines);
 		line1_adapter = new ArrayAdapter<String>
 		(this, android.R.layout.simple_spinner_dropdown_item, line1); 
 		line2_adapter = new ArrayAdapter<String>
 		(this, android.R.layout.simple_spinner_dropdown_item, line2);
-		
-		
-		
+	}
+
+	void setSpinnerListener() {
 		lines_sp.setOnItemSelectedListener(new OnItemSelectedListener() {
-		    @Override
-		    public void onItemSelected(AdapterView<?> parent, View arg1,
-		            int position, long id) {
-		    	
-		    	switch(position) {
-		    	case 0 : 
-		    		departure_sp.setAdapter(line1_adapter);
-		    		destination_sp.setAdapter(line1_adapter);
-		    		break;
-		    	case 1 :
-		    		departure_sp.setAdapter(line2_adapter);
-		    		destination_sp.setAdapter(line2_adapter);
-		    		break;
-		    	default :
-		    		departure_sp.setAdapter(null);
-		    		destination_sp.setAdapter(null);
-		    		break;
-		    	}
-		    }
-		    
-		    @Override
-		    public void onNothingSelected(AdapterView<?> parent) {
-		    
-		    }
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View arg1,
+					int position, long id) {
+
+				switch(position) {
+				case 0 : 
+					departure_sp.setAdapter(line1_adapter);
+					destination_sp.setAdapter(line1_adapter);
+					break;
+				case 1 :
+					departure_sp.setAdapter(line2_adapter);
+					destination_sp.setAdapter(line2_adapter);
+					break;
+				default :
+					departure_sp.setAdapter(null);
+					destination_sp.setAdapter(null);
+					break;
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
 		});	
-		
+
+		departure_sp.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View arg1,
+					int position, long id) {
+
+				station_count = (int)Math.abs(departure_sp.getSelectedItemId() - destination_sp.getSelectedItemId());
+
+				count.setText("출발 : " + departure_sp.getSelectedItem().toString() + 
+						"\n도착 : " + destination_sp.getSelectedItem().toString() +
+						"\n역 개수 : " + station_count);
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+		});
+
+		destination_sp.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View arg1,
+					int position, long id) {
+
+				station_count = (int)Math.abs(departure_sp.getSelectedItemId() - destination_sp.getSelectedItemId());
+
+				count.setText("출발 : " + departure_sp.getSelectedItem().toString() + 
+						"\n도착 : " + destination_sp.getSelectedItem().toString() +
+						"\n역 개수 : " + station_count);
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+		});
+
+		start.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				second = 0;
+
+				minute = station_count * 2;
+
+				Thread thread1 = new Thread(new Runnable() {
+					public void run() {
+
+						while(minute != 0 || second != 0) {
+
+							try{
+								Thread.sleep(1000);
+							} catch(Exception e) {
+								e.printStackTrace();
+							}
+							
+							if(second == 0) {
+								minute--;
+								second = 60;
+							}
+
+							handler.post(new Runnable() {
+								public void run() {
+									timer.setText("" + minute + " 분 " + second + " 초 남았습니다.");		
+								}
+							});
+							
+							second--;
+						}
+					}
+				}); 
+				thread1.start();
+			}
+		});
+
 	}
 
 	@Override
